351eb94a1bfb0b6db0d74e19259fea82c670acea
PTR\jrbis <jrbistuer@gmail.com>
2023-07-27 20:35:00 +0200

    new id and publish

---
+++ android/app/src/main/java/com/jrbistuer/samplecifo05/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jrbistuer.samplecifo05;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'appsample01',
+  appId: 'com.jrbistuer.samplecifo05',
+  appName: 'samplecifo05',
   webDir: 'www',
   server: {
     androidScheme: 'https'
